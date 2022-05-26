c6bf5b856b3b9024bcc99e1e678fa2e46bef20f3
Alex Betke <arhontes@yandex.ru>
2022-05-26 19:06:39 +0300

    refactored

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,46 +1,41 @@
 import React, {ChangeEvent, useState} from 'react';
 import s from './counter.module.css'
 import UniversalButton from "../Button/UniversalButton";
+import Display from "../Display/Display";
+import IncAndReset from "../IncAndReset/IncAndReset";
 
 
 function Counter() {
     const [num, setNum] = useState<number>(0)
-
+    const maxValue = 5
+    const minValue = 0
 
     const incHandler = () => {
-        if (num < 5) {
-            setNum(num+1)
+        if (num < maxValue) {
+            setNum(num + 1)
         }
     }
-
-    const onKeyPressHandler = (e:React.KeyboardEvent<HTMLButtonElement>)=>{
-        if(e.key==="+"){
+    /*const onKeyPressHandler = (e: React.KeyboardEvent<HTMLButtonElement>) => {
+        if (e.key === "+") {
             incHandler()
+        } else if (e.key === "-" && num > 0) {
+            setNum(num - 1)
         }
-        else if(e.key==="-"&&num>0){
-            setNum(num-1)
-        }
-    }
+    }*/
 
 
     return (
-
-            <div className={s.counter}>
-
-                <div className={s.display}>
-                    <div className={`${s.count} ${num===5&&s.limit}`}> {num} </div>
-                </div>
-
-                <div className={s.inputArea}>
-                   <UniversalButton onKeyPress={onKeyPressHandler} disabled={num === 5} onClick={incHandler} name={"inc"}></UniversalButton>
-                   <UniversalButton disabled={num === 0} onClick={() => setNum(0)} name={"reset"}></UniversalButton>
-                </div>
-
-            </div>
-
-
+        <div className={s.counter}>
+            <Display value={num}/>
+            <IncAndReset value={num}
+                         maxValue={maxValue}
+                         minValue={minValue}
+                         incHandler={incHandler}
+                         setNum={setNum}/>
+        </div>
 
     );
 }
 
 export default Counter;
+
