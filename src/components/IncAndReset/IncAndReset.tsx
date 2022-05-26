26c79feaf222ef435cac2847294e2df664f75e48
Alex Betke <arhontes@yandex.ru>
2022-05-26 19:05:52 +0300

    added IncAndReset function component

---
+++ src/components/IncAndReset/IncAndReset.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import s from "../Counter/counter.module.css";
+import UniversalButton from "../Button/UniversalButton";
+
+type IncAndResetPropsType = {
+    value:number
+    maxValue:number
+    minValue:number
+    incHandler:()=>void
+    setNum: (value:number)=>void
+}
+
+function IncAndReset(props:IncAndResetPropsType) {
+    return (
+        <div className={s.inputArea}>
+            <UniversalButton disabled={props.value === props.maxValue} onClick={props.incHandler}
+                             name={"inc"}></UniversalButton>
+            <UniversalButton disabled={props.value === props.minValue} onClick={() => props.setNum(props.minValue)} name={"reset"}></UniversalButton>
+        </div>
+    );
+}
+
+export default IncAndReset;
