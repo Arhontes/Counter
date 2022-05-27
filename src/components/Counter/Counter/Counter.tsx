137189a8e226314118d9fca89f1117e4c67006f9
Alex Betke <arhontes@yandex.ru>
2022-05-27 22:46:45 +0300

    added text error message

--- src/components/Counter/Counter/Counter.tsx
+++ src/components/Counter/Counter/Counter.tsx
@@ -4,32 +4,27 @@ import Display from "../Display/Display";
 import IncAndReset from "../IncAndReset/IncAndReset";
 
 
-function Counter() {
-    const [value, setValue] = useState<number>(0)
-    const maxValue = 5
-    const minValue = 0
+type CounterPropsType ={
+    maxValue:number
+    minValue:number
+    currentValue:number
+    incHandler:()=>void
+    setValue:(minValue:number)=>void
+}
 
-    const incHandler = () => {
-        if (value < maxValue) {
-           getFromLocalStorageHandler()
-        }
-    }
+function Counter(props:CounterPropsType) {
 
-    const setToLocalStorageHandler = () => {
-        localStorage.setItem('counterValue', `${value}`)
-    }
-    setToLocalStorageHandler()
 
-    const getFromLocalStorageHandler = () => {
-        let valueAsString = localStorage.getItem('counterValue')
-        if(valueAsString)setValue(parseInt(valueAsString) +1)
-    }
 
     return (
         <div className={s.counter}>
-            <Display value={value}/>
-            <IncAndReset value={value} maxValue={maxValue} minValue={minValue} incHandler={incHandler}
-                         setNum={setValue}/>
+            <Display maxValue={props.maxValue} value={props.currentValue}/>
+
+            <IncAndReset value={props.currentValue}
+                         maxValue={props.maxValue}
+                         minValue={props.minValue}
+                         incHandler={props.incHandler}
+                         resetValue={props.setValue}/>
         </div>
     );
 }
