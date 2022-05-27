89d39a2fb82b34ca76e5320886caf4af29ece642
Alex Betke <arhontes@yandex.ru>
2022-05-27 11:59:24 +0300

    refactored

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -6,34 +6,21 @@ import IncAndReset from "../IncAndReset/IncAndReset";
 
 
 function Counter() {
-    const [num, setNum] = useState<number>(0)
+    const [value, setValue] = useState<number>(0)
     const maxValue = 5
     const minValue = 0
 
     const incHandler = () => {
-        if (num < maxValue) {
-            setNum(num + 1)
+        if (value < maxValue) {
+            setValue(value + 1)
         }
     }
-    /*const onKeyPressHandler = (e: React.KeyboardEvent<HTMLButtonElement>) => {
-        if (e.key === "+") {
-            incHandler()
-        } else if (e.key === "-" && num > 0) {
-            setNum(num - 1)
-        }
-    }*/
-
 
     return (
         <div className={s.counter}>
-            <Display value={num}/>
-            <IncAndReset value={num}
-                         maxValue={maxValue}
-                         minValue={minValue}
-                         incHandler={incHandler}
-                         setNum={setNum}/>
+            <Display value={value}/>
+            <IncAndReset value={value} maxValue={maxValue} minValue={minValue} incHandler={incHandler} setNum={setValue}/>
         </div>
-
     );
 }
 
