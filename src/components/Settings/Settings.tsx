1b09556c046fd983ee71a962168879919a5c639f
Alex Betke <arhontes@yandex.ru>
2022-05-27 22:44:41 +0300

    unable strict mode

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,42 +1,34 @@
-import React, {ChangeEvent, useState} from 'react';
-
+import React, {ChangeEvent} from 'react';
+import s from './Settings.module.css'
 export type SettingsPropsType={
-
+    maxValue:number
+    minValue:number
+    setMaxValueHandler:(el:ChangeEvent<HTMLInputElement>)=>void
+    setMinValueHandler:(el:ChangeEvent<HTMLInputElement>)=>void
+    setSettingsToLocalStorage:()=>void
 }
 
 function Settings(props:SettingsPropsType) {
 
-    const [minMaxValue,setMinMaxValue] = useState([0,0])
-    const [minValue, maxValue] = minMaxValue
-    
-    const setMaxValueHandler = (el:ChangeEvent<HTMLInputElement>)=>{
-        if(+el.currentTarget.value>=minValue){
-            const tempMaxValue = parseInt(el.currentTarget.value)
-            setMinMaxValue([minValue, tempMaxValue])
-        }
-        else{
-            alert("can't set maxValue")
-        }
-
-
-    }
-    const setMinValueHandler = (el:ChangeEvent<HTMLInputElement>)=>{
-        if(+el.currentTarget.value<maxValue){
-            const tempMinValue = parseInt(el.currentTarget.value)
-            setMinMaxValue([tempMinValue, maxValue])
-        }
-        else{
-            alert("can't set minValue")
-        }
-
-
-    }
-
 
     return (
-        <div>
-            <input onChange={setMaxValueHandler} value={maxValue} type={"number"}/>
-            <input onChange={setMinValueHandler}  value={minValue} type={"number"}/>
+        <div className={s.settings}>
+            <div className={s.inputZone}>
+                <div className={s.setZone}>
+                    <div className={s.description}>Max Value</div>
+                    <div><input onChange={props.setMaxValueHandler} value={props.maxValue} type={"number"}/></div>
+                </div>
+               <div className={s.setZone}>
+                   <div className={s.description}>Min Value</div>
+                   <input onChange={props.setMinValueHandler}  value={props.minValue} type={"number"}/>
+               </div>
+
+            </div>
+
+            <div className={s.setButton}>
+                <button onClick={props.setSettingsToLocalStorage}>Set</button>
+            </div>
+
         </div>
     );
 }
